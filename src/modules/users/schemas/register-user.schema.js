4325c9261ec0c8d84f57f176bb1cc7dbc4bf080a
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2021-08-29 03:45:39 -0500

    fix: fixing register schema

--- src/modules/users/schemas/register-user.schema.js
+++ src/modules/users/schemas/register-user.schema.js
@@ -3,9 +3,9 @@ import * as Yup from 'yup';
 export const registerUser = Yup.object().shape({
   fullName: Yup.string().min(5).required(),
   email: Yup.string().email().required(),
+  phoneNumber: Yup.string().length(10).required(),
   password: Yup.string().min(6).required(),
   confirmPassword: Yup.string()
     .required()
-    .oneOf([Yup.ref('password')], 'Passwords do not match'),
-  phone: Yup.string().length(10).required()
+    .oneOf([Yup.ref('password')], 'Passwords do not match')
 });
